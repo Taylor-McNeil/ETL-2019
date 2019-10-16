23c8c17db6354932c1dd3ee20ff28cf87c62ba7c
James Nguyen <chamie@Jamess-MacBook-Pro.local>
2019-10-16 17:07:08 -0400

    10/16 update

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,10 +6,10 @@ import '../static/bootstrap.min.css';
 const Home = () => (
   <div>
       <Head>
-          <title>Flex'n</title>
+          <title>Flex</title>
       </Head>
       <div className="jumbotron text-center">
-          <h1 className="display-4">Welcome to Flex'n</h1>
+          <h1 className="display-4">Welcome to F /_ E X</h1>
           <p className="lead">An application that automates data retrieval and facilitates data management.</p>
           <hr className="my-4"/>
               <p>Frankie | Kamil | Taylor | James</p>
