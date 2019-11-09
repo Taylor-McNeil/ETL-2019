d04ca15bee79157315eb1f0c4afdaa3ef7932935
Kamil Rutkowski <kamilr97@gmail.com>
2019-11-09 14:33:10 -0500

    Added rudimentary POST/GET abilities

--- Team4/ETLFlex/rules/urls.py
+++ Team4/ETLFlex/rules/urls.py
@@ -1,7 +1,16 @@
 from rest_framework import routers
+from django.conf.urls import url
+from django.urls import path, include
 from .api import RulesViewSet
+from rules import views
 
 router = routers.DefaultRouter()
 router.register('api/rules', RulesViewSet, 'rules')
+#router.register(r'^admin/', admin.site.urls)
+#router.register('testPost/', views.testClass, 'testClass')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    url('testPost/', views.testPost),    
+    url('testGet/', views.testGet)
+]
