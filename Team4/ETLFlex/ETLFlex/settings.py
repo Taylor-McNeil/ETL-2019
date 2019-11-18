73adff7a57d93516477c975cdf8d3790d7e6439f
Kamil Rutkowski <kamilr97@gmail.com>
2019-11-17 19:24:02 -0500

    hotfix; Readded previously removed code in settings.py

--- Team4/ETLFlex/ETLFlex/settings.py
+++ Team4/ETLFlex/ETLFlex/settings.py
@@ -80,6 +80,7 @@ WSGI_APPLICATION = 'ETLFlex.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
